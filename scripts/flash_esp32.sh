35b6e1581552a75096ea619fbb841c8962c32b6a
Alexander Komyakov <agkomiakov@hex.team>
2025-04-06 16:03:10 +0300

    add esptool to deb

--- scripts/flash_esp32.sh
+++ scripts/flash_esp32.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-esptool.py --port /dev/ttyUSB0 write_flash \
+
+/opt/esp32-tools/esptool.py --port /dev/ttyUSB0 write_flash \
     0x1000 /opt/esp32-project/bootloader.bin \
     0x8000 /opt/esp32-project/partition-table.bin \
     0x10000 /opt/esp32-project/firmware.bin
