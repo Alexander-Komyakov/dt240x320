0c93ad52c7975ac795f708f5bcef1d35b5e1e5a9
Alexander Komyakov <agkomiakov@hex.team>
2025-05-06 00:31:13 +0300

    doodle update image and move button

--- main/doodle.c
+++ main/doodle.c
@@ -4,13 +4,25 @@
 void game_doodle(spi_device_handle_t spi) {
     xStreamBuffer = xStreamBufferCreate(STREAM_BUF_SIZE, sizeof(int));
 
-    fill_screen_gradient(spi, 0x0000, 0xFFFF);
+    fill_screen(spi, 0xFFFF);
+    draw_image(spi, &image_doodle_hero);
 
     int received_button;
+    uint8_t speed = 3;
     while (1) {
         // Обработка ввода игрока
         if (xStreamBufferReceive(xStreamBuffer, &received_button, sizeof(received_button), 0) > 0) {
-            vTaskDelay(100 / portTICK_PERIOD_MS);
+            if (received_button == BUTTON_UP) {
+                image_doodle_hero.y = (image_doodle_hero.y > speed) ? image_doodle_hero.y - speed : 0;
+                draw_image(spi, &image_doodle_hero);
+            }
+            else if (received_button == BUTTON_DOWN) {
+                image_doodle_hero.y = (image_doodle_hero.y < DISPLAY_HEIGHT - image_doodle_hero.height - speed) ?
+                          image_doodle_hero.y + speed : DISPLAY_HEIGHT - image_doodle_hero.height;
+                draw_image(spi, &image_doodle_hero);
+            }
+
+            vTaskDelay(10 / portTICK_PERIOD_MS);
         }
     }
 }
---
+++ main/doodle_hero.h
@@ -0,0 +1,4 @@
+#include "image_structure.h"
+
+extern const uint16_t image_doodle_hero_pixels[3024];
+extern Image image_doodle_hero;
