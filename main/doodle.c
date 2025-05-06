2208f45386f5d1786c7b6460a5d40e35fa493e57
Alexander Komyakov <agkomiakov@hex.team>
2025-05-06 21:51:43 +0300

    update doodle move

--- main/doodle.c
+++ main/doodle.c
@@ -8,27 +8,27 @@ void game_doodle(spi_device_handle_t spi) {
     draw_image(spi, &image_doodle_hero);
 
     int received_button;
-    uint8_t speed = 3;
+    uint8_t speed = 1;
     while (1) {
         // Обработка ввода игрока
         if (xStreamBufferReceive(xStreamBuffer, &received_button, sizeof(received_button), 0) > 0) {
             if (received_button == BUTTON_UP) {
-                if (image_doodle_hero.y > 0) {
+                if (image_doodle_hero.y > speed) {
                     image_doodle_hero.y = image_doodle_hero.y - speed;
                 } else {
-                    image_doodle_hero.y = DISPLAY_HEIGHT;
+                    image_doodle_hero.y = DISPLAY_HEIGHT + 15;
                 }
                 draw_image(spi, &image_doodle_hero);
             }
             else if (received_button == BUTTON_DOWN) {
-                if (image_doodle_hero.y < DISPLAY_HEIGHT) {
+                if (image_doodle_hero.y < DISPLAY_HEIGHT + 15) {
                     image_doodle_hero.y = image_doodle_hero.y + speed;
                 } else {
                     image_doodle_hero.y = 0;
                 }
                 draw_image(spi, &image_doodle_hero);
             }
-            vTaskDelay(10 / portTICK_PERIOD_MS);
+            vTaskDelay(5 / portTICK_PERIOD_MS);
         }
     }
 }
