93457980375512540684d7a938b619e9f9996383
Alexander Komyakov <agkomiakov@hex.team>
2025-05-07 17:11:08 +0300

    add jump to doodle

--- main/doodle.c
+++ main/doodle.c
@@ -8,7 +8,9 @@ void game_doodle(spi_device_handle_t spi) {
     draw_image(spi, &image_doodle_hero);
 
     int received_button;
-    uint8_t speed = 1;
+    uint8_t speed = 3;
+    uint8_t speed_jump = 1;
+    uint8_t limit_jump = 80;
     while (1) {
         // Обработка ввода игрока
         if (xStreamBufferReceive(xStreamBuffer, &received_button, sizeof(received_button), 0) > 0) {
@@ -28,7 +30,16 @@ void game_doodle(spi_device_handle_t spi) {
                 }
                 draw_image(spi, &image_doodle_hero);
             }
-            vTaskDelay(10 / portTICK_PERIOD_MS);
         }
+        if (limit_jump != 0) {
+            image_doodle_hero.x += speed_jump;
+            limit_jump -= speed_jump;
+        } else if (image_doodle_hero.x > 0) {
+            image_doodle_hero.x -= speed_jump;
+        } else {
+            limit_jump = 80;
+        }
+        draw_image(spi, &image_doodle_hero);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
